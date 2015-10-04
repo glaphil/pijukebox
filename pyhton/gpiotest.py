e1cb9b4a52491058a92fd843fb9c888ca217230f
phil_pi <glaphilippe@gmail.com>
2015-10-04 21:07:02 +0200

    modif doc + gpiotrst

--- pyhton/gpiotest.py
+++ pyhton/gpiotest.py
@@ -12,11 +12,11 @@ print("start")
 try:  
 	while True:  
 		GPIO.output(4, 1)         # set GPIO4 to 1/GPIO.HIGH/True  
-		sleep(0.5)                 # wait half a second  
+		sleep(1)                 # wait half a second  
 		if GPIO.input(4):  
 			print("LED just about to switch off")  
 		GPIO.output(4, 0)         # set GPIO4 to 0/GPIO.LOW/False  
-		sleep(0.5)                 # wait half a second  
+		sleep(1)                 # wait half a second  
 		if not GPIO.input(4):  
 			print("LED just about to switch on")  
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  

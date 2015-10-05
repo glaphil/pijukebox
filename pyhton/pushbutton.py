0c6f96644162ae91979345d55ce079fa311bc2fa
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-10-05 21:36:23 +0200

    add simple push button test

---
+++ pyhton/pushbutton.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+print("simple button push test")    
+
+try:  
+  while True:  
+    if GPIO.input(7):               
+      print("button down, gpio HIGH")
+    else:
+      print("button up, gpio DOWN")
+
+    #if GPIO.input(11):               
+    #  print("button up, gpio HIGH")
+    #else:
+    #  print("button down, gpio DOWN") 
+
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+  print("end")

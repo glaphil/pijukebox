606b93d498641575e31e3881251e2f53c3bbfcce
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-10-06 22:10:51 +0200

    add test for pull down and pull up button

--- pyhton/test.py
+++
@@ -1,9 +0,0 @@
-from time import sleep             # lets us have a delay  
-  
-try:  
-	while True:  
-		print("test")  
-		sleep(0.5)                 # wait half a second  
-except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
-	print("end")
-

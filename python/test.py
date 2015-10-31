682fa758a9fcdca3b9b03da16fb573829dcbd72c
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-10-31 10:46:54 +0100

    fix script

--- python/test.py
+++ python/test.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python2.7  
-# demo of "BOTH" bi-directional edge detection  
-# script by Alex Eames http://RasPi.tv  
-# http://raspi.tv/?p=6791  
   
 import RPi.GPIO as GPIO  
 from time import sleep     # this lets us have a time delay (see line 12)  
   
-GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(6, GPIO.IN,pull_up_down=GPIO.PUD_UP)    # set GPIO25 as input (button)  
+# set up BCM GPIO numbering
+GPIO.setmode(GPIO.BCM)     
+# set gpio 6 as input with pull up resistor
+GPIO.setup(6, GPIO.IN,pull_up_down=GPIO.PUD_UP)    
 
 # Define a threaded callback function to run in another thread when events are detected  
 def my_callback(channel):  
-    if GPIO.input(6):     # if port 25 == 1  
-        print("Rising edge detected on 25")  
-    else:                  # if port 25 != 1  
-        print("Falling edge detected on 25")  
+    if GPIO.input(6):    
+        print("Rising edge detected)  
+    else:                 
+        print("Falling edge detected)  
   
-# when a changing edge is detected on port 25, regardless of whatever   
-# else is happening in the program, the function my_callback will be run  
-GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback, bouncetime=50)  
+GPIO.add_event_detect(6, GPIO.BOTH, callback=my_callback, bouncetime=500)  
     
 try:  
-    sleep(30)         # wait 30 seconds  
+    sleep(30)   
   
-finally:                   # this block will run no matter how the try block exits  
-    GPIO.cleanup()         # clean up after yourself  
+finally:                   
+    GPIO.cleanup()           
 

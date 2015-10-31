dcf291eb119cffa76c20d93b5db4f49e540ebb42
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-10-31 10:25:51 +0100

    fix script

--- python/test.py
+++ python/test.py
@@ -7,7 +7,7 @@ import RPi.GPIO as GPIO
 from time import sleep     # this lets us have a time delay (see line 12)  
   
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
-GPIO.setup(6, GPIO.IN,pull_up_down=GPIO.PUD_UP))    # set GPIO25 as input (button)  
+GPIO.setup(6, GPIO.IN,pull_up_down=GPIO.PUD_UP)    # set GPIO25 as input (button)  
 
 # Define a threaded callback function to run in another thread when events are detected  
 def my_callback(channel):  

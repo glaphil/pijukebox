e136e46ce411c10100402428b98224ec431acbbe
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-11-06 23:06:01 +0100

    fix

--- python/buttonsToKeysWithCallbacksPigPio.py
+++ python/buttonsToKeysWithCallbacksPigPio.py
@@ -109,7 +109,7 @@ def shudtown():
   import subprocess
   process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
   output = process.communicate()[0]
-  print output
+  print(output)
 
 try:  
   print("Init GPIO mappings callbacks")

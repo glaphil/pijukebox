d7050e889a1ed63bd25e5dd6c92eed8da30e4efc
Vasconcelos Philippe <glaphilippe@gmail.com>
2015-11-06 21:41:46 +0100

    pigpio version

---
+++ python/buttonsToKeysWithCallbacksPigPio.py
@@ -0,0 +1,99 @@
+import pigpio
+import time  
+import uinput
+
+# buttons are wired from gpio pin, pulled up resistor (3.3V), to ground
+# we detect falling edges
+
+# pigpio use BCM numering
+
+# set bounce time to avoid multiple press in microseconds
+GLITCH_FILTER=50000
+
+# pins names / bcm gpio
+INSERT_COIN = 4
+PREVIOUS = 17
+NEXT = 18
+STOP = 27
+SONG_1 = 6
+SONG_2 = 12
+SONG_3 = 13
+SONG_4 = 16  
+SONG_5 = 19
+SONG_6 = 20
+SONG_7 = 26
+SONG_8 = 21
+
+# keys press 
+keys = {
+  uinput.KEY_C,
+  uinput.KEY_P,
+  uinput.KEY_N,
+  uinput.KEY_S,
+  uinput.KEY_KP1,
+  uinput.KEY_KP2,
+  uinput.KEY_KP3,
+  uinput.KEY_KP4,
+  uinput.KEY_KP5,
+  uinput.KEY_KP6,
+  uinput.KEY_KP7,
+  uinput.KEY_KP8
+}
+
+# mappings between pins and key press (tuples)
+mappings = [
+  (INSERT_COIN,uinput.KEY_C),
+  (PREVIOUS, uinput.KEY_P),
+  (NEXT, uinput.KEY_N),
+  (STOP, uinput.KEY_S),
+  (SONG_1, uinput.KEY_KP1),
+  (SONG_2, uinput.KEY_KP2),
+  (SONG_3, uinput.KEY_KP3),
+  (SONG_4, uinput.KEY_KP4),
+  (SONG_5, uinput.KEY_KP5),
+  (SONG_6, uinput.KEY_KP6),
+  (SONG_7, uinput.KEY_KP7),
+  (SONG_8, uinput.KEY_KP8)
+]
+
+callbacks = []
+
+device = uinput.Device(keys)
+
+# because GPIO.FALLING detect both failling and rising edge, need to set this variable to fix that issue
+ignore_key_release_fix = False
+
+pi = pigpio.pi() # Connect to local Pi.
+
+def init_pin_mapping(pinKey):
+  pi.set_pull_up_down(pinKey[0], pigpio.PUD_UP)   
+  pi.set_mode(pinKey[0], pigpio.INPUT) 
+  pi.set_glitch_filter(pinKey[0], GLITCH_FILTER)
+  callbacks.append(pi.callback(pinKey[0], pigpio.EITHER_EDGE, emitKey))
+
+  #GPIO.add_event_detect(pinKey[0], GPIO.FALLING, callback=lambda x: emitKey(pinKey), bouncetime=BOUNCE_TIME) 
+
+
+def emitKey(pinKey, level, tick):
+   if level == 1: 
+      print(pinKey[1]," released")
+      print("Rising edge detected at", tick)
+      device.emit(pinKey[1],0) # release
+   else: 
+      print(pinKey[1]," pressed")                
+      print("Falling edge detected at", tick)
+      device.emit(pinKey[1],1) # press
+
+try:  
+  for pinKey in mappings:
+    print("map ",pinKey[0]," to ",pinKey[1])
+    init_pin_mapping(pinKey)
+  while True:
+    time.sleep(1)
+except KeyboardInterrupt:
+  # clean up GPIO on CTRL+C exit  
+  GPIO.cleanup()    
+  for cb in callbacks:
+    cb.cancel() # Cancel callback.
+  pi.stop() # Disconnect from local Pi.
+     
